d6b742953ec69b1ca08fb2b40aff3842956b2882
Robert <99199842+Robert-Nogueira@users.noreply.github.com>
2025-08-03 17:30:22 -0300

    🧹 chore(handler.rs): remove commented-out code to clean up the handler implementation and improve readability

--- src/handler.rs
+++ src/handler.rs
@@ -11,18 +11,6 @@ pub struct Handler {
     pub snipers: HashMap<u64, Sniper>,
 }
 
-// if let Some(guild_id) = msg.guild_id {
-//     let guild = if let Some(guild) = guild_id.to_guild_cached(&ctx.cache) {
-// 	guild
-//     } else {
-// 	match guild_id.to_partial_guild(&ctx.http).await {
-// 	    Ok(partial) => partial.into(),
-// 	    Err(_) => return
-// 	}
-//     };
-// };
-// println!("{:?}", msg.guild_id);
-
 #[async_trait]
 impl EventHandler for Handler {
     async fn message(&self, _ctx: Context, msg: Message) {
