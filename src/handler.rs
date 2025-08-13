d3f1225cec6455e76c5ebcd5fced279b11c17408
Robert Nogueira <contact@robertnogueira.com>
2025-08-13 06:53:04 -0300

    ✨ feat(handler.rs): implement setup_snipers function to initialize snipers for configured channels and improve message handling logic

--- src/handler.rs
+++ src/handler.rs
@@ -1,19 +1,39 @@
-use serenity_self::all::{Context, EventHandler, Message, async_trait};
+use std::sync::Arc;
 
-use crate::{settings::SETTINGS, snipers::SNIPERS};
+use serenity_self::{
+    all::{Context, EventHandler, Http, Message, async_trait},
+    http,
+};
+use tokio::sync::Mutex;
+
+use crate::{
+    settings::SETTINGS,
+    snipers::{SNIPERS, Sniper},
+};
 
 pub struct Handler {}
+
+fn setup_snipers(http: Arc<Http>) {
+    let channels = SETTINGS.channels_ids.clone();
+    let mut sniper: Arc<Mutex<Sniper>>;
+    for channel_id in channels {
+        sniper = Arc::new(Mutex::new(Sniper::new(
+            channel_id,
+            SETTINGS.guild_id,
+            Arc::clone(&http),
+        )));
+        SNIPERS.insert(channel_id, Arc::clone(&sniper));
+    }
+}
 
 #[async_trait]
 impl EventHandler for Handler {
     async fn message(&self, ctx: Context, msg: Message) {
-        let current_user_id = ctx.cache.current_user().id;
+        if msg.author.id == SETTINGS.client_id && msg.content.as_str() == "!start" {
+            setup_snipers(Arc::clone(&ctx.http))
+        }
         if let Some(sniper) = SNIPERS.get(&msg.channel_id.into()) {
             let sniper = sniper.lock().await;
-
-            if msg.author.id == current_user_id && msg.content != "!start" {
-                // setup()
-            }
 
             if !sniper.running {
                 return;
