2e054692434392404f8f2b5d4af98bb051db6cc6
Robert Nogueira <contact@robertnogueira.com>
2025-08-13 09:19:29 -0300

    ✨ feat(handler.rs): add snipe_kakera functionality for snipers in event handler

--- src/handler.rs
+++ src/handler.rs
@@ -53,5 +53,10 @@ impl EventHandler for Handler {
         if !SETTINGS.channels_ids.contains(&channel_id) || msg.author.id != 432610292342587392 {
             return;
         }
+
+        if let Some(sniper) = SNIPERS.get(&channel_id) {
+            let sniper = sniper.lock().await;
+            sniper.snipe_kakera(&ctx, &msg).await;
+        }
     }
 }
