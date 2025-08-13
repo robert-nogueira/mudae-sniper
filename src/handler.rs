018d3b3c068c859eb98c0d177abb2a8af221ad81
Robert <99199842+Robert-Nogueira@users.noreply.github.com>
2025-08-13 06:15:53 -0300

    🐛 fix(handler.rs): update message handler logic to check if the author is the current user before executing setup

--- src/handler.rs
+++ src/handler.rs
@@ -7,11 +7,13 @@ pub struct Handler {}
 #[async_trait]
 impl EventHandler for Handler {
     async fn message(&self, ctx: Context, msg: Message) {
-        if msg.guild_id.is_none() && msg.content != "!start" {
-            // setup();
-        }
+        let current_user_id = ctx.cache.current_user().id;
         if let Some(sniper) = SNIPERS.get(&msg.channel_id.into()) {
             let sniper = sniper.lock().await;
+
+            if msg.author.id == current_user_id && msg.content != "!start" {
+                // setup()
+            }
 
             if !sniper.running {
                 return;
