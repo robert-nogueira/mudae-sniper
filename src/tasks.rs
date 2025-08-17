604451f96374ce59dd03d62512ed56331269f0ba
Robert <99199842+Robert-Nogueira@users.noreply.github.com>
2025-08-17 07:24:26 -0300

    ♻️ refactor: change DateTime type from Utc to Tz for better timezone handling in statistics and related functions

--- src/tasks.rs
+++ src/tasks.rs
@@ -1,39 +1,35 @@
 use std::{sync::Arc, time::Duration as TimeDuration};
 
-use chrono::{DateTime, Duration, Utc};
-use regex::Regex;
+use chrono::{DateTime, Duration};
+use chrono_tz::Tz;
 use serenity_self::{
     all::{
-        Context, Message, MessageCollector, Reaction, ReactionCollector,
-        ReactionType, ShardMessenger,
+        Message, MessageCollector, Reaction, ReactionCollector, ReactionType,
+        ShardMessenger,
     },
     futures::StreamExt,
 };
 use tokio::{sync::Mutex, time::sleep};
 
 use crate::{
-    snipers::{Sniper, sniper},
-    utils::REGEX_GET_NUMBERS,
+    snipers::Sniper,
+    utils::{REGEX_GET_NUMBERS, get_local_time},
 };
-
-// pub async fn roll(sniper: Arc<Mutex<Sniper>>) {
-//     loop {
-
-//     }
-// }
 
 pub async fn daily_claimer(
     sniper_mutex: Arc<Mutex<Sniper>>,
     shard: ShardMessenger,
 ) {
-    let mut next_daily: DateTime<Utc>;
+    let mut next_daily: DateTime<Tz>;
     let mut running: bool;
     {
         let sniper = sniper_mutex.lock().await;
         next_daily = sniper.statistics.next_daily;
         running = sniper.running;
     }
+
     const CHECK_INTERVAL: TimeDuration = TimeDuration::from_secs(60);
+
     loop {
         while !running {
             sleep(CHECK_INTERVAL).await;
@@ -41,21 +37,23 @@ pub async fn daily_claimer(
             next_daily = sniper.statistics.next_daily;
             running = sniper.running;
         }
-        sleep(
-            (next_daily - Utc::now())
-                .to_std()
-                .unwrap()
-                .max(TimeDuration::ZERO),
-        )
-        .await;
+
+        let wait_duration = (next_daily - get_local_time())
+            .to_std()
+            .unwrap_or(TimeDuration::ZERO);
+
+        sleep(wait_duration).await;
+
         let (channel_id, http) = {
             let sniper = sniper_mutex.lock().await;
             (sniper.channel_id, sniper.http.clone())
         };
+
         channel_id
             .say(&http, "$daily")
             .await
             .expect("fail on send &daily");
+
         let mut collector = ReactionCollector::new(&shard)
             .channel_id(channel_id)
             .author_id(432610292342587392.into())
@@ -65,11 +63,12 @@ pub async fn daily_claimer(
                 _ => false,
             })
             .stream();
+
         match collector.next().await {
-            Some(_) => {
+            Some(reaction) => {
                 let mut sniper = sniper_mutex.lock().await;
                 sniper.statistics.next_daily =
-                    Utc::now() + Duration::hours(20);
+                    get_local_time() + Duration::hours(20);
                 next_daily = sniper.statistics.next_daily;
                 running = sniper.running;
             }
@@ -88,34 +87,37 @@ pub async fn daily_kakera_claimer(
         text.replace(",", "").replace(".", "").parse().ok()
     }
 
-    let mut next_daily: DateTime<Utc>;
+    let mut next_dk: DateTime<Tz>;
     let mut running: bool;
     {
         let sniper = sniper_mutex.lock().await;
-        next_daily = sniper.statistics.next_daily;
+        next_dk = sniper.statistics.next_dk;
         running = sniper.running;
     }
+
     const CHECK_INTERVAL: TimeDuration = TimeDuration::from_secs(60);
+
     loop {
         while !running {
             sleep(CHECK_INTERVAL).await;
             let sniper = sniper_mutex.lock().await;
-            next_daily = sniper.statistics.next_daily;
+            next_dk = sniper.statistics.next_dk;
             running = sniper.running;
         }
-        sleep(
-            (next_daily - Utc::now())
-                .to_std()
-                .unwrap()
-                .max(TimeDuration::ZERO),
-        )
-        .await;
+
+        let wait_duration = (next_dk - get_local_time())
+            .to_std()
+            .unwrap_or(TimeDuration::ZERO);
+
+        sleep(wait_duration).await;
+
         let (channel_id, http) = {
             let sniper = sniper_mutex.lock().await;
             (sniper.channel_id, sniper.http.clone())
         };
+
         channel_id
-            .say(&http, "$daily")
+            .say(&http, "$dk")
             .await
             .expect("fail on send &daily");
         let mut collector = MessageCollector::new(&shard)
@@ -124,6 +126,7 @@ pub async fn daily_kakera_claimer(
             .timeout(TimeDuration::from_secs(30))
             .filter(move |m: &Message| REGEX_GET_NUMBERS.is_match(&m.content))
             .stream();
+
         match collector.next().await {
             Some(msg) => {
                 let stock_value = REGEX_GET_NUMBERS
@@ -134,9 +137,9 @@ pub async fn daily_kakera_claimer(
                 let mut sniper = sniper_mutex.lock().await;
                 sniper.statistics.kakera_stock =
                     stock_value.unwrap_or(sniper.statistics.kakera_stock);
-                sniper.statistics.next_daily =
-                    Utc::now() + Duration::hours(20);
-                next_daily = sniper.statistics.next_daily;
+                sniper.statistics.next_dk =
+                    get_local_time() + Duration::hours(20);
+                next_dk = sniper.statistics.next_dk;
                 running = sniper.running;
             }
             None => {
