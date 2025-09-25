6dd55f9120fa05497991e21191fcbbddcb7129d1
Robert <99199842+Robert-Nogueira@users.noreply.github.com>
2025-09-24 21:05:41 -0300

    ♻️ refactor(tasks): refactored tasks file structure

--- src/tasks/daily_kakera_claimer.rs
+++ src/tasks/daily_kakera_claimer.rs
@@ -3,10 +3,7 @@ use std::{sync::Arc, time::Duration as TimeDuration};
 use chrono::{DateTime, Duration};
 use chrono_tz::Tz;
 use serenity_self::{
-    all::{
-        Message, MessageCollector, Reaction, ReactionCollector, ReactionType,
-        ShardMessenger,
-    },
+    all::{Message, MessageCollector, ShardMessenger},
     futures::StreamExt,
 };
 use tokio::{sync::Mutex, time::sleep};
@@ -16,70 +13,7 @@ use crate::{
     utils::{REGEX_GET_NUMBERS, get_local_time},
 };
 
-pub async fn daily_claimer(
-    sniper_mutex: Arc<Mutex<Sniper>>,
-    shard: ShardMessenger,
-) {
-    let mut next_daily: DateTime<Tz>;
-    let mut running: bool;
-    {
-        let sniper = sniper_mutex.lock().await;
-        next_daily = sniper.statistics.next_daily;
-        running = sniper.running;
-    }
-
-    const CHECK_INTERVAL: TimeDuration = TimeDuration::from_secs(60);
-
-    loop {
-        while !running {
-            sleep(CHECK_INTERVAL).await;
-            let sniper = sniper_mutex.lock().await;
-            next_daily = sniper.statistics.next_daily;
-            running = sniper.running;
-        }
-
-        let wait_duration = (next_daily - get_local_time())
-            .to_std()
-            .unwrap_or(TimeDuration::ZERO);
-
-        sleep(wait_duration).await;
-
-        let (channel_id, http) = {
-            let sniper = sniper_mutex.lock().await;
-            (sniper.channel_id, sniper.http.clone())
-        };
-
-        channel_id
-            .say(&http, "$daily")
-            .await
-            .expect("fail on send &daily");
-
-        let mut collector = ReactionCollector::new(&shard)
-            .channel_id(channel_id)
-            .author_id(432610292342587392.into())
-            .timeout(TimeDuration::from_secs(30))
-            .filter(move |r: &Reaction| match &r.emoji {
-                ReactionType::Unicode(unicode) => unicode == "✅",
-                _ => false,
-            })
-            .stream();
-
-        match collector.next().await {
-            Some(reaction) => {
-                let mut sniper = sniper_mutex.lock().await;
-                sniper.statistics.next_daily =
-                    get_local_time() + Duration::hours(20);
-                next_daily = sniper.statistics.next_daily;
-                running = sniper.running;
-            }
-            None => {
-                continue;
-            }
-        }
-    }
-}
-
-pub async fn daily_kakera_claimer(
+pub async fn daily_kakera_claimer_task(
     sniper_mutex: Arc<Mutex<Sniper>>,
     shard: ShardMessenger,
 ) {
