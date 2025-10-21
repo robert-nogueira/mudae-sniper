903d87fed7217060349bcdae97010cdd99956594
Robert Nogueira <contact@robertnogueira.com>
2025-10-21 04:31:06 -0300

    ✨ feat(errors): add CaptureError enum to handle button-related errors in sniping functionality

--- src/snipers/mod.rs
+++ src/snipers/mod.rs
@@ -1,13 +1,14 @@
+pub mod errors;
 pub mod sniper;
 pub mod statistics;
 
+use dashmap::DashMap;
 use std::sync::{Arc, LazyLock};
 
-use dashmap::DashMap;
-
-use serenity_self::all::ChannelId;
 pub use sniper::Sniper;
 pub use statistics::Statistics;
+
+use serenity_self::all::ChannelId;
 use tokio::sync::Mutex;
 
 pub static SNIPERS: LazyLock<DashMap<ChannelId, Arc<Mutex<Sniper>>>> =
