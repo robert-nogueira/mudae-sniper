0db4837bff0eaeff2b1f3f45ac37a690da545701
Robert <99199842+Robert-Nogueira@users.noreply.github.com>
2025-10-22 12:45:17 -0300

    ♻️ refactor(settings): move mudae_prefix from SniperSettings to Mudae settings for clarity

--- src/settings/sniper_confg.rs
+++ src/settings/sniper_confg.rs
@@ -5,7 +5,6 @@ pub struct SniperSettings {
     pub guild_id: u64,
     pub channels_ids: Vec<u64>,
     pub roll_command: String,
-    pub mudae_prefix: String,
     pub capture_threshold: u32,
     pub roll_after_claim: bool,
 }
