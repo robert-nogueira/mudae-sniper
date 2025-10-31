4bb72b76db4cb53d5c9aa9227061bfeeb94a4130
Robert <99199842+Robert-Nogueira@users.noreply.github.com>
2025-10-31 13:34:21 -0300

    ♻️ refactor(sniper): replace channels_ids with channels config struct for improved clarity and access to channel names

--- src/settings/sniper_confg.rs
+++ src/settings/sniper_confg.rs
@@ -1,9 +1,15 @@
 use serde::Deserialize;
 
 #[derive(Debug, Deserialize)]
+pub struct ChannelConfig {
+    pub name: String,
+    pub id: u64,
+}
+
+#[derive(Debug, Deserialize)]
 pub struct SniperSettings {
     pub guild_id: u64,
-    pub channels_ids: Vec<u64>,
+    pub channels: Vec<ChannelConfig>,
     pub roll_command: String,
     pub capture_threshold: u32,
     pub roll_after_claim: bool,
