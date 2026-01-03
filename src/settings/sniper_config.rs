64f9974a2a3eceff7e61eec96cfc8607fbdcde7e
Robert <contact@robertnogueira.com>
2026-01-03 19:09:15 -0300

    ✨ feat(sniper_config): add roll_after_claim with default value to SniperSettings

--- src/settings/sniper_config.rs
+++ src/settings/sniper_config.rs
@@ -6,12 +6,17 @@ pub struct InstanceConfig {
     pub id: u64,
 }
 
+fn default_roll_after_claim() -> bool {
+    false
+}
+
 #[derive(Debug, Deserialize)]
 pub struct SniperSettings {
     pub guild_id: u64,
     pub instances: Vec<InstanceConfig>,
     pub roll_command: String,
     pub capture_threshold: u32,
+    #[serde(default = "default_roll_after_claim")]
     pub roll_after_claim: bool,
     pub rt_capture_threshold: u32,
 }
